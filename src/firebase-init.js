5d06dcbf14fdc48e6c5b281290c6e3c0acc109e1
Denis Koshman <dikay.et@gmail.com>
2019-07-05 13:06:25 +0200

    resolved vulnerability issue

--- src/firebase-init.js
+++ src/firebase-init.js
@@ -1,11 +1,7 @@
 import firebase from "firebase";
 
-const config = {
-	apiKey: "<AIzaSyAvFIafXdqrmn8f0u94y8jbqRvBdYLqFB0",
-	authDomain: "simple-14d89.firebaseapp.com",
-	databaseURL: "https://simple-14d89.firebaseio.com",
-	storageBucket: "simple-14d89.appspot.com",
-};
+const config = {};
+
 firebase.initializeApp(config);
 
 export default firebase;
